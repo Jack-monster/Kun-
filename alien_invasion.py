910d7f0619bd4e0a8e81e9b213cc062ddab028eb
MonsterJack <1811663227@qq.com>
2023-09-22 00:02:10 +0800

    cool！

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
-
 import pygame
+
+from settings import Settings
+from ikun import Kunkun
 
 class AlienInvasion:
     """管理游戏资源和行为"""
@@ -9,9 +11,11 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings=Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-    
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
@@ -19,7 +23,7 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+            self.screen.fill(self.settings.bg_color)
             #让最近绘制的屏幕可见
             pygame.display.flip()
     
---
+++ ikun.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Kunkun:
+    """管理坤坤的类"""
+    def __init__(self,aigame):
+        """初始化坤坤并管理其位置"""
+        self.screen=aigame.screen
+        self.screen_rect=aigame.screen.get_rect()
+
+        #加载飞船
+        self.image=pygame.image.load('img/ji1.bmp')
+        self.rect=self.image.get_rect()
+
+        self.rect.midbottom=self.screen_rect.midbottom
+
+    def blitme(self):
+        """绘制坤"""
+        self.screen.blit(self.image,self.rect)

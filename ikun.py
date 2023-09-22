44f6a7362548c3aa94b5dc70189f8e2f682ae2d0
MonsterJack <1811663227@qq.com>
2023-09-22 11:41:00 +0800

    让坤坤可以上下移动，并且投出篮球

--- ikun.py
+++ ikun.py
@@ -6,13 +6,39 @@ class Kunkun:
         """初始化坤坤并管理其位置"""
         self.screen=aigame.screen
         self.screen_rect=aigame.screen.get_rect()
+        self.settings=aigame.settings
 
-        #加载飞船
+        #加载坤坤
         self.image=pygame.image.load("C:\\Users\\86182\\Desktop\\python_work\\alien_invation_game\\img\\ji1.bmp")
         self.rect=self.image.get_rect()
 
+        #初始化位置
         self.rect.midbottom=self.screen_rect.midbottom
+
+        #位置属性
+        self.x=float(self.rect.x)
+        self.y=float(self.rect.y)
+
+        #移动属性
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def blitme(self):
         """绘制坤"""
         self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        """更新坤的状态"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x +=self.settings.kunkun_speed_x
+        if self.moving_left and self.rect.left > 0:
+            self.x -=self.settings.kunkun_speed_x
+        self.rect.x=self.x
+
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y +=self.settings.kunkun_speed_y
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -=self.settings.kunkun_speed_y
+        self.rect.y=self.y
--- settings.py
+++ settings.py
@@ -3,3 +3,7 @@ class Settings:
         self.screen_width=1200
         self.screen_height=800
         self.bg_color=(230,230,230)
+        self.kunkun_speed_x=1.5
+        self.kunkun_speed_y=0.5
+        self.ball_speed=0.5
+        self.ball_maxnum = 7
